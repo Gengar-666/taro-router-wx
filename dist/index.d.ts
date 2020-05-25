ababd2d0ce6fa57378a205a33b9b3d86b8909baf
obito <lvy.123@foxmail.com>
2020-05-25 16:21:16 +0800

    add注解

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,12 +3,39 @@ import { BeforeEachHookCallBack, Location } from './types';
 declare class TaroRouter {
     history: History;
     constructor();
+    /**
+     * 普通跳转
+     * @param {Location} location
+     */
     push(location: Location): void;
+    /**
+     * 重定向
+     * @param {Location} location
+     */
     replace(location: Location): void;
+    /**
+     * tabbar页面跳转
+     * @param {Location} location
+     */
     switchTab(location: Location): void;
+    /**
+     * 关闭所有页面，打开到应用内的某个页面
+     * @param {Location} location
+     */
     relaunch(location: Location): void;
+    /**
+     * 返回
+     */
     back(): void;
+    /**
+     * 路由跳转前的钩子函数
+     * @param beforeEachHookCallBack hook回调函数
+     */
     beforeEach(beforeEachHookCallBack: BeforeEachHookCallBack): void;
+    /**
+     * 自动把router实例挂载到Componet原型上。
+     * @param componet 组件实例
+     */
     register(componet: any): void;
 }
 export default TaroRouter;
--- dist/index.js
+++ dist/index.js
@@ -9,24 +9,51 @@ class TaroRouter {
     constructor() {
         this.history = new history_1.default();
     }
+    /**
+     * 普通跳转
+     * @param {Location} location
+     */
     push(location) {
         this.history.push(location);
     }
+    /**
+     * 重定向
+     * @param {Location} location
+     */
     replace(location) {
         this.history.replace(location);
     }
+    /**
+     * tabbar页面跳转
+     * @param {Location} location
+     */
     switchTab(location) {
         this.history.switchTab(location);
     }
+    /**
+     * 关闭所有页面，打开到应用内的某个页面
+     * @param {Location} location
+     */
     relaunch(location) {
         this.history.relaunch(location);
     }
+    /**
+     * 返回
+     */
     back() {
         this.history.back();
     }
+    /**
+     * 路由跳转前的钩子函数
+     * @param beforeEachHookCallBack hook回调函数
+     */
     beforeEach(beforeEachHookCallBack) {
         hooks_1.default.beforeEachHookCallBack = beforeEachHookCallBack;
     }
+    /**
+     * 自动把router实例挂载到Componet原型上。
+     * @param componet 组件实例
+     */
     register(componet) {
         componet.prototype.$router = this;
     }
